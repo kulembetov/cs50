ee5fcae3495feee90e42f0a99c4bd5b08f0e9a66
Artur Kulembetov <artur_kulembetov@outlook.com>
2025-04-05 17:10:38 +0700

    success: week-8

---
+++ week-8/homepage/constants.js
@@ -0,0 +1,17 @@
+// personal information
+const USER_INFO = {
+    name: 'Artur Kulembetov',
+    email: 'artur.kulembetov@gmail.com',
+    location: 'Remote / Available for relocation',
+    availability: 'Open to new opportunities',
+    social: {
+        github: 'https://github.com/kulembetov',
+        linkedin: 'https://linkedin.com/in/kulembetov',
+        twitter: 'https://x.com/arturkulembetov',
+        telegram: 'https://t.me/artur_kulembetov',
+        instagram: 'https://instagram.com/arturkulembetov',
+        devto: 'https://dev.to/kulembetov',
+        medium: 'https://medium.com/@kulembetov',
+        codepen: 'https://codepen.io/kulembetov'
+    }
+}; 
